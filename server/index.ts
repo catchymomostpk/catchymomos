48088a0f575cb1384a83bcb18996405504289d55
jeyasurya <Jeyasuryat@student.tce.edu>
2025-10-07 14:40:46 +0530

    Add CORS for separate deployments

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
